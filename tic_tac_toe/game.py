354e495c3e19e54bcdf7a1aa7a78be9bc1df3660
Мария  Мутрук <mariamutruk72@gmail.com>
2026-04-16 09:56:58 +0300

    __name__ == '__main__':

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -1,7 +1,10 @@
 from gameparts import Board
 
-game = Board()
-game.display()
-game.make_move(1, 1, 'X')
-print('Ход сделан!')
-game.display() 
+# Всё, что ниже этой инструкции, не будет импортироваться,
+# но будет выполняться при запуске файла game.py.
+if __name__ == '__main__':
+    game = Board()
+    game.display()
+    game.make_move(1, 1, 'X')
+    print('Ход сделан!')
+    game.display() 
